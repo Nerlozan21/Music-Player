fbfa93584137d4820a8ed18a27fccc025624833a
RajBabu <harshraut22@gmail.com>
2023-09-01 17:27:12 +0530

    initialized the project

---
+++ README.md
@@ -0,0 +1,8 @@
+# Music-Player
+Features to add:
+Suffle button,
+Equlizer,
+Index of Songs
+
+Bugs:
+When song ends it does not automatically plays the next songs
---
+++ js/music-list.js
@@ -0,0 +1,38 @@
+let allMusic = [
+  {
+    name: "Animals",
+    artist: "Marron 5",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "A Sky Full of Stars",
+    artist: "Coldplay",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Bailando",
+    artist: "Enrique Iglesias",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Bolevard of Broken Dreams",
+    artist: "Green Day",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "Counting Stars",
+    artist: "One Republic",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Adventure of Lifetime",
+    artist: "Coldplay",
+    img: "music-6",
+    src: "music-6"
+  },
+];
